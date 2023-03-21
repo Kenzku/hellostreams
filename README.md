efbe06de6b53c89afe1e37fa5afae7f666f9ec7c
F K <ken.fuguo@gmail.com>
2023-03-22 00:07:51 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# hellostreams
+# hellostreams
---
+++ src/test/java/com/ibm/hellostreams/HellostreamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.hellostreams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellostreamsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
